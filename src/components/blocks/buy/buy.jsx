e49f85c70d60e7b0e7acb17fa43ccbd513119685
Zaur <0lezgin5gmail.com>
2024-05-01 10:52:14 +0500

    реализует компонент Accordion и стилизует его

--- src/components/blocks/buy/buy.jsx
+++ src/components/blocks/buy/buy.jsx
@@ -3,12 +3,37 @@ import { Form, Label, Li, Ul } from "../../../components/styled";
 import Button from "../../ui/button/button";
 import { TitleSize } from "../../ui/title/title";
 import RadioButton from "../../ui/radio-button/radio-button";
-import { StyledSection, Price, RadioLabelForTime, FormItem, StyleTitle } from "./styles";
+import { Accordion } from "../../ui/accordion/accordion";
+import {
+  StyledSection,
+  Price,
+  AccordionText,
+  RadioLabelForTime,
+  FormItem,
+  StyleTitle,
+  RadioLabelForType
+} from "./styles";
 
 // страница покупки
 function Buy({ buyOptions }) {
-  const { durationOptions } = buyOptions;
+  const { durationOptions, ticketOptions } = buyOptions;
   const [duration, setDuration] = useState(durationOptions[0]);
+  const [selectType, setSelectType] = useState(ticketOptions[0].id);
+  const accordionContent = ticketOptions.map((option) => ({
+    id: option.id,
+    title: (
+      <RadioButton
+        labelComponent={RadioLabelForType}
+        selectValue={selectType}
+        value={option.id}
+        text={option.title}
+        onChange={(el) => {
+          setSelectType(Number(el.target.value));
+        }}
+      />
+    ),
+    description: option.description
+  }));
   return (
     <StyledSection>
       <StyleTitle level={1} size={TitleSize.BIG}>
@@ -35,6 +60,11 @@ function Buy({ buyOptions }) {
         </FormItem>
         <FormItem $bottom={22}>
           <Label $margin={12}>Тип билета</Label>
+          <Accordion
+            textComponent={AccordionText}
+            content={accordionContent}
+            isHtml
+          />
         </FormItem>
         <Label $small $margin={6}>
           Цена
