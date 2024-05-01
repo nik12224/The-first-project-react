c1750f2b7bcddaf92e7e856073e5474bd989dcc5
Zaur <0lezgin5gmail.com>
2024-05-01 11:01:37 +0500

    реализует вычисление цены, оформление, обработка крайних случаев

--- src/components/blocks/buy/buy.jsx
+++ src/components/blocks/buy/buy.jsx
@@ -15,63 +15,86 @@ import {
 } from "./styles";
 
 // страница покупки
-function Buy({ buyOptions }) {
-  const { durationOptions, ticketOptions } = buyOptions;
+function Buy({ buyOptions: { durationOptions = [], ticketOptions = [] } }) {
+  const canBuy = durationOptions.length && ticketOptions.length;
   const [duration, setDuration] = useState(durationOptions[0]);
   const [selectType, setSelectType] = useState(ticketOptions[0].id);
-  const accordionContent = ticketOptions.map((option) => ({
-    id: option.id,
-    title: (
-      <RadioButton
-        labelComponent={RadioLabelForType}
-        selectValue={selectType}
-        value={option.id}
-        text={option.title}
-        onChange={(el) => {
-          setSelectType(Number(el.target.value));
-        }}
-      />
-    ),
-    description: option.description
-  }));
+
+  const type = ticketOptions.find((option) => option.id === selectType);
+  const price = duration * (type ? type.price : 0);
+
+  // преобразуем ticketOptions в данные для аккордеона
+  const accordionContent =
+    canBuy &&
+    ticketOptions.map((option) => ({
+      id: option.id,
+      title: (
+        <RadioButton
+          labelComponent={RadioLabelForType}
+          selectValue={selectType}
+          value={option.id}
+          text={option.title}
+          onChange={(el) => {
+            setSelectType(Number(el.target.value));
+          }}
+        />
+      ),
+      description: option.description
+    }));
+
+  const handleBuy = () => {
+    alert(
+      `Спасибо за покупку.
+              Вы приобрели билет класса: ${type.title}.
+              Продолжительность посещения(часов): ${duration}.
+              Цена билета: ${price} руб.`
+    );
+  };
+
   return (
     <StyledSection>
       <StyleTitle level={1} size={TitleSize.BIG}>
         Купить билет
       </StyleTitle>
-      <Form $width={540}>
-        <FormItem>
-          <Label $margin={12}>Продолжительность (часов)</Label>
-          <Ul $isGridList $indent={12} $align="left">
-            {durationOptions.map((option) => (
-              <Li key={option}>
-                <RadioButton
-                  labelComponent={RadioLabelForTime}
-                  selectValue={duration}
-                  value={option}
-                  text={option}
-                  onChange={(el) => {
-                    setDuration(Number(el.target.value));
-                  }}
-                />
-              </Li>
-            ))}
-          </Ul>
-        </FormItem>
-        <FormItem $bottom={22}>
-          <Label $margin={12}>Тип билета</Label>
-          <Accordion
-            textComponent={AccordionText}
-            content={accordionContent}
-            isHtml
-          />
-        </FormItem>
-        <Label $small $margin={6}>
-          Цена
-        </Label>
-        <Price>500 руб.</Price>
-        <Button minWidth={460}>Купить билет</Button>
-      </Form>
+      {canBuy ? (
+        <Form $width={540}>
+          <FormItem>
+            <Label $margin={12}>Продолжительность (часов)</Label>
+            <Ul $isGridList $indent={12} $align="left">
+              {durationOptions.map((option) => (
+                <Li key={option}>
+                  <RadioButton
+                    labelComponent={RadioLabelForTime}
+                    selectValue={duration}
+                    value={option}
+                    text={option}
+                    onChange={(el) => {
+                      setDuration(Number(el.target.value));
+                    }}
+                  />
+                </Li>
+              ))}
+            </Ul>
+          </FormItem>
+          <FormItem $bottom={22}>
+            <Label $margin={12}>Тип билета</Label>
+            <Accordion
+              textComponent={AccordionText}
+              content={accordionContent}
+              isHtml
+            />
+          </FormItem>
+          <Label $small $margin={6}>
+            Цена
+          </Label>
+          <Price>{price} руб.</Price>
+          <Button minWidth={460} onClick={handleBuy}>
+            Купить билет
+          </Button>
+        </Form>
+      ) : (
+        <p>Покупка билетов временно недоступна</p>
+      )}
     </StyledSection>
   );
 }
--- src/components/ui/button/button.jsx
+++ src/components/ui/button/button.jsx
@@ -1,12 +1,12 @@
 import React, { forwardRef } from "react";
 import { StyledButton } from "./styles";
 
-const Button = forwardRef(({ children, minWidth, link, className }, ref) => {
+const Button = forwardRef(({ children, minWidth, link, className, onClick }, ref) => {
   return (
     <StyledButton
       ref={ref}
       $minWidth={minWidth}
-      {...(link ? { href: link } : { as: "button", type: "button" })}
+      {...(link ? { href: link } : { as: "button", onClick, type: "button" })}
       className={className}
     >
       {children}
